6e47a25abbeb3d0ef163d445900b92750cdceb4a
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-03 13:52:44 +0100

    Log connection.

--- Source/Scanner3DFirmware/main.cpp
+++ Source/Scanner3DFirmware/main.cpp
@@ -1,13 +1,16 @@
 #include "pch.h"
 #include "Networking/Server/WebSocketServer.h"
+#include <iostream>
 
 int main()
 {
+    Networking::Initialize();
+
     const auto server = Networking::IWebSocketServer::CreateWebby(50485, 1);
 
     server->SetOnClientConnectedCallback([](const auto& connection)
     {
-        return true;
+        std::cout << "Someone is connected";
     });
 
     server->Initialize();
@@ -16,5 +19,7 @@ int main()
         server->Update();
 
     server->Finalize();
+    Networking::Finalize();
+
     return 0;
 }
