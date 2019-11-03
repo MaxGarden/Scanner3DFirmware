f15032e3fce7a015dcd862b76d209282ad8ee2d1
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-03 13:02:11 +0100

    Creating server in main.

--- Source/Scanner3DFirmware/main.cpp
+++ Source/Scanner3DFirmware/main.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include "Networking/Server/WebSocketServer.h"
 
 int main()
 {
+    const auto server = Networking::IWebSocketServer::CreateWebby(50485, 1);
+
+    server->SetOnClientConnectedCallback([](const auto& connection)
+    {
+        return true;
+    });
+
+    server->Initialize();
+
+    while (true)
+        server->Update();
+
+    server->Finalize();
     return 0;
 }
