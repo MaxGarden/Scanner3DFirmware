d87c2acd137e616d643677b926f6f8979231005b
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-09 18:53:49 +0100

    Added firmware header.

---
+++ Source/Scanner3DFirmware/Scanner3DFirmware.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <assert.h>
+
+namespace Scanner3DFirmware
+{
+    #define FIRMWARE_ASSERT(expression) assert(expression)
+}
--- Source/pch.h
+++ Source/pch.h
@@ -7,6 +7,8 @@
 
 #ifdef __cplusplus
 
+#include "Scanner3DFirmware.h"
+
 #endif //__cplusplus
 
 #endif //_STDAFX_H_
