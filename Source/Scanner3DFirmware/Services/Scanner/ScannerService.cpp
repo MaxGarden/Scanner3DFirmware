e2f18de64d86b0fa0ab10edd5467a1c5eb6e6dc7
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-11 16:00:19 +0100

    Added average points request handler to scanner service.

--- Source/Scanner3DFirmware/Services/Scanner/ScannerService.cpp
+++ Source/Scanner3DFirmware/Services/Scanner/ScannerService.cpp
@@ -9,13 +9,33 @@ bool ScannerService::Initialize()
 {
     auto result = RequestResponseServiceBase::Initialize();
 
+    static const auto binarizedData = []()
+    {
+        auto result = Camera::Capture();
+        Scanner::PerformBinarization(result);
+
+        return result;
+    };
+
     result &= RegisterRequestHandler('b', [](auto&& payload)
     {
-        auto responsePayload = Camera::Capture();
-        Scanner::PerformBinarization(responsePayload);
+        auto responsePayload = binarizedData();
         const auto responseType = responsePayload.empty() ? Response::ResponseType::Fail : Response::ResponseType::Ok;
 
         return Response{ responseType, std::move(responsePayload) };
+    });
+
+    result &= RegisterRequestHandler('a', [](auto&& payload)
+    {
+        const auto cameraConfig = Camera::GetConfig();
+        const auto averagePoints = Scanner::CalculateAveragePoints(binarizedData(), cameraConfig.Width);
+
+        RemoteServices::ServicePayload responsePayload;
+
+        const auto beginIterator = reinterpret_cast<const byte*>(averagePoints.data());
+        const auto endIterator = beginIterator + averagePoints.size() * sizeof(decltype(averagePoints)::value_type);
+
+        return Response{ Response::ResponseType::Ok, RemoteServices::ServicePayload{ beginIterator, endIterator } };
     });
 
     FIRMWARE_ASSERT(result);
