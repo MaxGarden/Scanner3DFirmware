a205089815749738c8cb860307178c863eff69fa
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-12-07 17:17:57 +0100

    Revert "Changed motor steps count from 6400 to 400."
    This reverts commit 710161e1e867f165855ef02a70ce9bb3947f6e00.

--- Source/Scanner3DFirmware/Tray/Tray.cpp
+++ Source/Scanner3DFirmware/Tray/Tray.cpp
@@ -8,7 +8,7 @@
 using namespace Scanner3DFirmware;
 
 static Tray::Config s_config;
-static const auto s_motorStepAngleInDegrees = 1.8f;
+static const auto s_motorStepAngleInDegrees = 0.05625f;
 
 void Tray::ApplyConfig(Config&& config)
 {
